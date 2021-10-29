cc2434a79c8f4b933a7f0115e18248e7d71517f7
Red Arrow <studio@redarrow.ltd>
2021-10-29 16:21:32 +0700

    CSS for detail

---
+++ public/main.2e6ad8437fbb0949ea61.hot-update.js
@@ -0,0 +1,2 @@
+"use strict";self.webpackHotUpdatemyth("main",{},(function(t){t.h=()=>"d0a03d0d5517e536d471"}));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi4yZTZhZDg0MzdmYmIwOTQ5ZWE2MS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI4REFBQUEsRUFBb0JDLEVBQUksSUFBTSIsInNvdXJjZXMiOlsid2VicGFjazovL215dGgvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIl9fd2VicGFja19yZXF1aXJlX18uaCA9ICgpID0+IChcImQwYTAzZDBkNTUxN2U1MzZkNDcxXCIpIl0sIm5hbWVzIjpbIl9fd2VicGFja19yZXF1aXJlX18iLCJoIl0sInNvdXJjZVJvb3QiOiIifQ==
