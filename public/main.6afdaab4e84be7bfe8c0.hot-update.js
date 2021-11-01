02c4aa522c1d86aaebe72750d65940d5e553656e
Red Arrow <studio@redarrow.ltd>
2021-11-01 15:07:02 +0700

    fixed journal

--- public/main.6afdaab4e84be7bfe8c0.hot-update.js
+++
@@ -1,2 +0,0 @@
-"use strict";self.webpackHotUpdatemyth("main",{},(function(e){e.h=()=>"2e6ad8437fbb0949ea61"}));
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi42YWZkYWFiNGU4NGJlN2JmZThjMC5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI4REFBQUEsRUFBb0JDLEVBQUksSUFBTSIsInNvdXJjZXMiOlsid2VicGFjazovL215dGgvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIl9fd2VicGFja19yZXF1aXJlX18uaCA9ICgpID0+IChcIjJlNmFkODQzN2ZiYjA5NDllYTYxXCIpIl0sIm5hbWVzIjpbIl9fd2VicGFja19yZXF1aXJlX18iLCJoIl0sInNvdXJjZVJvb3QiOiIifQ==
